3301f082045874d4abea717ea29c5d87057d2ff6
ggperroni <guilhermegasparperroni97@gmail.com>
2025-06-26 17:35:35 -0300

    refactor: deleted unused imports

--- composeApp/src/commonMain/kotlin/com/miga/piggy/App.kt
+++ composeApp/src/commonMain/kotlin/com/miga/piggy/App.kt
@@ -3,12 +3,8 @@ package com.miga.piggy
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.transitions.CrossfadeTransition
-import cafe.adriel.voyager.transitions.ScaleTransition
 import cafe.adriel.voyager.transitions.SlideTransition
-import com.miga.piggy.auth.presentation.ui.AuthScreen
 import com.miga.piggy.splash.SplashScreen
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinContext
 
 @Composable
