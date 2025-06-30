ae64d81d9bf58ff50c3614a34b88b8d59fdff0df
ggperroni <guilhermegasparperroni97@gmail.com>
2025-06-30 02:00:13 -0300

    refactor: deleted unused imports

--- composeApp/src/commonMain/kotlin/com/miga/piggy/home/presentation/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/miga/piggy/home/presentation/HomeScreen.kt
@@ -16,8 +16,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.List
