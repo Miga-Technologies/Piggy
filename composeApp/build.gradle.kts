0f37db95d9bc960bc0fd5a0bf3812d1569fde45f
ggperroni <guilhermegasparperroni97@gmail.com>
2025-07-20 01:22:39 -0300

    refactor: added gitignore rule

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -98,6 +98,9 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    lint {
+        disable.add("NullSafeMutableLiveData")
+    }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
